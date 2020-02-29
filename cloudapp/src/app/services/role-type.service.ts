3c34fe97a9e1a6f2eb6e2a47974c4c4b0f6e2373
gpeterso <gpeterso@umn.edu>
2020-02-29 10:08:26 -0600

    Cache RoleTypeService response

--- cloudapp/src/app/services/role-type.service.ts
+++ cloudapp/src/app/services/role-type.service.ts
@@ -1,31 +1,42 @@
-import { Injectable } from '@angular/core';
-import { Observable, fromEventPattern, concat } from 'rxjs';
-import { map, skip, pluck } from 'rxjs/operators';
-import { sortBy, partialRight } from 'lodash';
-import { CloudAppRestService } from '@exlibris/exl-cloudapp-angular-lib';
-import { RoleType } from '../models/user';
-import { CodeTable, CodeTableRow} from '../models/code-table';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map, tap, pluck, shareReplay } from "rxjs/operators";
+import { sortBy } from "lodash";
+import { CloudAppRestService } from "@exlibris/exl-cloudapp-angular-lib";
+import { RoleType } from "../models/user";
+import { CodeTable, CodeTableRow } from "../models/code-table";
 
+const toRoleTypes = (rows: CodeTableRow[]): RoleType[] =>
+  rows.map(row => ({ code: row.code, desc: row.description }));
 
-const toRoleTypes = (rows: CodeTableRow[]): RoleType[] => 
-  rows.map(row => ({code: row.code, desc: row.description}))
-
-const sortByDesc = partialRight(sortBy, 'desc');
-
-/**
- */
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RoleTypeService {
+  private cache$: Observable<RoleType[]>;
+
   constructor(private restService: CloudAppRestService) {}
 
+  /**
+   * Fetch the role types from the HFrUserRoles.roleType code table,
+   * sorted by desc. The result is cached until the app is reloaded.
+   */
   get(): Observable<RoleType[]> {
-    return this.restService.call('/conf/code-tables/HFrUserRoles.roleType')
-    .pipe(
-      map(resp => resp as CodeTable),
-      pluck('row'),
-      map(toRoleTypes),
-      map(sortByDesc));
+    if (!this.cache$) {
+      this.cache$ = this.loadData().pipe(shareReplay(1));
+    }
+    return this.cache$;
+  }
+
+  private loadData() {
+    return this.restService
+      .call("/conf/code-tables/HFrUserRoles.roleType")
+      .pipe(
+        map(resp => resp as CodeTable),
+        pluck("row"),
+        map(toRoleTypes),
+        map(roleTypes => sortBy(roleTypes, "desc")),
+        tap(rt => console.debug("fetched role types: ", rt))
+      );
   }
 }
